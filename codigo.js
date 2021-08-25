f2b11b3f584ca1ca80d353d2c95384e07580a766
sebastian beltran zapata <sebastian199502@hotmail.com>
2021-08-24 20:32:49 -0500

    JavaScript-Ejercicio-19

---
+++ codigo.js
@@ -0,0 +1,9 @@
+let numero1 = Number(prompt("Introduce un numero"));
+let numero2 = Number(prompt("Introduce otro numero"));
+
+function suma(numero1, numero2) {
+    let operacion = numero1 + numero2;
+    console.log(operacion);
+}
+
+suma(numero1, numero2);
